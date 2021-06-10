c0d5e503245ef4eb8394b37821708240d49f6084
Vlad <ilin96inbox@gmail.com>
2021-06-10 21:14:37 +0900

    deploy to rinkeby

--- deploy.js
+++ deploy.js
@@ -8,3 +8,20 @@ const provider = new HDWalletProvider(
 );
 
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Deploy", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there"],
+    })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("deploy to", result.options.address);
+};
+
+deploy();
