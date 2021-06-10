0c20eff34b94d5342a3336aac999213077cf5087
Vlad <ilin96inbox@gmail.com>
2021-06-10 19:20:50 +0900

    test message updates

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -29,4 +29,10 @@ describe("Inbox", () => {
     const message = await inbox.methods.message().call();
     assert.equal(message, INITIAL_MESSAGE);
   });
+
+  it("can change the message", async () => {
+    await inbox.methods.setMessage("new").send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "new");
+  });
 });
