1a3af59dcf6be09670c642f14e3d60372ad6b680
Vlad <ilin96inbox@gmail.com>
2021-06-10 18:48:18 +0900

    fetch account from ganache

--- compile.js
+++ compile.js
@@ -5,4 +5,4 @@ const solc = require("solc");
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inboxPath, "utf8");
 
-solc.compile(source, 1);
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
